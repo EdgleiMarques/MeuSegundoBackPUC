a93d925f8d44e58c93941379023544afac71e7be
Edglei Marques <edgleitab@gmail.com>
2022-10-11 15:08:38 -0300

    Adicionar arquivos de projeto.

---
+++ MeuPrimeiroBackPUC/Models/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroBackPUC.Models
+{
+	[Table("Veiculos")]
+	public class Veiculo
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required(ErrorMessage = "É obrigatório informar um modelo")]
+		public string Modelo { get; set; }
+		[Required(ErrorMessage = "É obrigatório informar uma placa do veiculo")]
+
+		public string Placa { get; set; }
+		public string Marca { get; set; }
+	}
+}
